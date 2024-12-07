6d43f8a31cd4a5cf25f44509bd9d373ed6e5c745
seokjunh <130536128+seokjunh@users.noreply.github.com>
2024-12-07 19:15:01 +0900

    Title: 순서대로 구구단, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241207/순서대로 구구단/multiplication-table-in-order.py
+++ 241207/순서대로 구구단/multiplication-table-in-order.py
@@ -2,7 +2,7 @@ a, b = map(int, input().split())
 
 if a > b:
     for j in range(1,10):
-        for i in range(a,b+1,-1):
+        for i in range(a,b-1,-1):
             print(i,"*",j,"=",i*j, end="  ")
         print()
 else:
