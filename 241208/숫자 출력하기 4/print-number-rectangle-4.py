f1b6f7355be4c40a8957a88cd1848ced0d0ec7a1
seokjunh <130536128+seokjunh@users.noreply.github.com>
2024-12-08 16:04:57 +0900

    Title: 숫자 출력하기 4, Time: 63ms, Memory: 18MB, Status: Passed - CodeTree

--- 241208/숫자 출력하기 4/print-number-rectangle-4.py
+++ 241208/숫자 출력하기 4/print-number-rectangle-4.py
@@ -11,12 +11,12 @@ elif k == 2:
     for i in range(n):
         if i % 2 == 0:
             for j in range(1,n+1):
-                if i == n:
+                if j == n:
                     print(j)
                 else:
                     print(j, end=" ")
         else:
-            for j in range(n,-1,-1):
+            for j in range(n,0,-1):
                 if j == 1:
                     print(j)
                 else:
