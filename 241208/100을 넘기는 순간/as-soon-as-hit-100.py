a3b175a30e93c7851f898abbe7ecf7d8f5eb4f1a
seokjunh <130536128+seokjunh@users.noreply.github.com>
2024-12-08 16:14:03 +0900

    Title: 100을 넘기는 순간, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241208/100을 넘기는 순간/as-soon-as-hit-100.py
+++ 241208/100을 넘기는 순간/as-soon-as-hit-100.py
@@ -2,5 +2,15 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-print(sum(arr))
-print(format(sum(arr)/n, ".1f"))
+total = 0
+cnt = 0
+for i in arr:
+    if i >= 100:
+        total += i
+        cnt += 1
+        break
+    total += i
+    cnt += 1
+
+print(total)
+print(format(total/cnt, ".1f"))
